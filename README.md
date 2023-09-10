0829ad93a933ae2c700bf362d3e1c94e635b3bc2
Sinskiy <sinskiy@proton.me>
2023-09-10 15:36:55 +0300

    update: 1.2

--- README.md
+++ README.md
@@ -6,18 +6,11 @@
   Seterra extra
 </h1>
 <p align="center">
-   Darkmode, no label and more cool features for Seterra 
+   Darkmode, no label and more cool features for Seterra
 </p>
 
-### Installation & usage guide
+### Installation guide (Chromium), Link (Firefox)
 
 > <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Chromium_Logo.svg/512px-Chromium_Logo.svg.png" alt="firefox logo" width="32px" height="32px" /> [For Chromium based: Chrome, Edge, Opera GX, Opera, Brave...](./chromium/README.md)
 
-> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Firefox_logo%2C_2019.svg/220px-Firefox_logo%2C_2019.svg.png" alt="firefox logo" width="32px" height="32px" /> [For Firefox based: Firefox, Tor (not sure if Tor supports extensions), Waterfox, LibreWolf...](https://addons.mozilla.org/ru/firefox/addon/seterraextra/)
-
-### Ideas
-
-*if you want one of these ideas to come to life or suggest an idea, open an issue*
-
-- Other custom map colors
-- Hoverclick
+> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Firefox_logo%2C_2019.svg/220px-Firefox_logo%2C_2019.svg.png" alt="firefox logo" width="32px" height="32px" /> [For Firefox based: Firefox, Tor, Waterfox, LibreWolf...](https://addons.mozilla.org/ru/firefox/addon/seterraextra/)
--- chromium/content.js
+++ chromium/content.js
@@ -1,246 +1,207 @@
-// run this every page load to make header dark if user chose dark quiz header and custom water
-const storageDarkHeaderOnUpdate = chrome.storage.local.get("dark_quiz_header")
-  .then((item) => {
-    acceptMessage("dark_quiz_header", item.dark_quiz_header)
-  });
+const labelBackgroundColor = '#333333'
+const lightGray = '#999999'
+const darkGray = '#333333'
+const gray = '#ebebeb'
+const background = '#232323'
 
-const storageWaterOnUpdate = chrome.storage.local.get("custom_water")
-  .then((item) => {
-    acceptMessage("custom_water", item.custom_water)
-  });
+const labelAreaSelector = '.game-area_tooltip__uwKTk:not(#createdLabelArea)'
+const labelSelector = labelAreaSelector + '>div'
+let manageLabelsInterval
 
+const cornerFlag = '.corner-image_wrapper__6gzbe'
+let manageFlagInterval
 
-let clickEventListener;
-let anotherClickEvenListener;
-let keyboardEventListener;
+const quizHeader = '[data-qa="game-map-header"]'
+let manageHeaderInterval
 
-let darkHeader = false;
-let darkMode = false;
+let manageThemeBigInterval
+let manageThemeSmallInterval
 
-let white = "#fff";
-let dark = "#232323";
-let lightGray = "#A0A0A0";
-let black = "#000";
+let manageLabelOpacityInterval
 
-// listens to messages (messages can only be send from popup.js on check)
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-      acceptMessage(message.command, message.value);
-});
+      acceptMessage(message.id, message.state);
+})
 
-// check message data
-function acceptMessage(changedInputID, inputValue) {
-  if (changedInputID === "no_label") {
-    if (inputValue === true) {
-      manageLabel(inputValue);
-    }
-  }
-
-  if (changedInputID === "no_corner_flag") {
-    manageCornerFlag(inputValue);
-  }
-
-  if (changedInputID === "dark_label") {
-    manageDarkLabel(inputValue);
-  }
-
-  if (changedInputID === "dark_quiz_header") {
-    manageHeader(inputValue);
-  }
-
-  if (changedInputID === "dark_mode") {
-    manageDarkMode(inputValue);
-  }
-
-  if (changedInputID === "custom_water") {
-    manageWater(inputValue);
-  }
-}
-
-function manageLabel(boolean) {
-  const label = ".game-tooltip_tooltip__iJM_6";
-  if (document.querySelector(label) !== null && boolean === true) {
-    document.querySelector(label).style.display = "none";
-  }
-}
-
-function manageCornerFlag(boolean) {
-  const cornerFlag = ".corner-image_wrapper__6gzbe";
-  if (document.querySelector(cornerFlag) !== null && boolean === true) {
-    document.querySelector(cornerFlag).style.display = "none";
-  }
-  else if (document.querySelector(cornerFlag) !== null && boolean === false) {
-    document.querySelector(cornerFlag).style.display = "flex";
-  }
-}
-
-function manageDarkLabel(boolean) {
-  const label = ".game-tooltip_tooltip__iJM_6";;
-  if (document.querySelector(label) !== null && boolean === true) {
-    document.querySelector(label).style.color = "white";
-    document.querySelector(label).style.background = "hsla(0,0%,0%,.7)";
-  }
-  else if (document.querySelector(label) !== null && boolean === false) {
-    document.querySelector(label).style.color = "black";
-    document.querySelector(label).style.background = "hsla(0,0%,100%,.9)";
-  }
-}
-
-function manageHeader(boolean) {
-  const header = document.querySelector(".game-header_wrapper__82t4V");
-  if (boolean === true && header !== null) {
-    header.style.background = "hsla(0,0%,0%,.3)";
-    header.style.color = "white";
-    darkHeader = true;
-  }
-  else if (boolean === false && header !== null) {
-    header.style.background = "hsla(0,0%,100%,.5)"
-    header.style.color = "black";
-    darkHeader = false;
-  }
-}
-
-function manageDarkMode(boolean, fromTimer) {
-  darkMode = boolean;
-
-  const backgroundBorders = document.querySelector(".seterra_content__IQ6lE");
-  const mainBackground = document.querySelector(".seterra_main__EGcCa");
-
-  const root = document.querySelector(":root");
-
-  const settings = document.querySelector(".game-footer_cog__ZBpOb");
-  const info = document.querySelector(".game-footer_infoIcon__pwBav");
-
-  const blueLinks = document.querySelectorAll(".games-list_linkItem__V_M6y a");
-  const buttons = document.querySelectorAll(".button_button__CnARx.button_variantSecondaryInverted__9kX_w, .button_link__xHa3x.button_variantSecondaryInverted__9kX_w");
-  if (boolean === true) {
-    backgroundBorders.style.background = dark;
-    backgroundBorders.style.color = white;
-    mainBackground.style.background = dark;
-
-    root.style.colorScheme = "dark";
-
-    blueLinks.forEach((link) => link.style.color = lightGray);
-    buttons.forEach((button) => {
-      button.style.color = white;
-      button.style.border = `.0625rem solid ${white}`;
+function getOptions() {
+  chrome.storage.local.get(null)
+    .then(options => {
+      for (let option in options) {
+        acceptMessage(option, options[option])
+      }
     })
+}
 
-    if (fromTimer === true) {
-      const buttonType = ".game-type-map-input_button__1K9uM"
-      const modal = ".modal_colorWhite__4hqsv";
-      const close = ".modal_closeButton__rq9h_ img";
-      const table = ".highscore_table__MNojq";
+function acceptMessage(id, state) {
+  if (id == 'labels') {
+    manageLabels(state)
+  }
+  else if (id == 'no_corner_flag') {
+    manageFlag(state)
+  }
+  else if (id == 'dark_quiz_header') {
+    manageHeader(state)
+  }
+  else if (id == 'dark_mode') {
+    manageTheme(state)
+  }
+  else if (id == 'label_opacity') {
+    manageLabelOpacity(state)
+  }
+}
 
-      if (document.querySelector(buttonType) !== null) document.querySelector(buttonType).style.color = dark;
+function manageLabels(state) {
+  clearInterval(manageLabelsInterval)
+  manageLabelsInterval = setInterval(changeLabels, 10, state);
+}
 
-      if (document.querySelector(modal) !== null) {
-        document.querySelector(modal).style.background = dark;
-        document.querySelector(modal).style.color = white;
-        document.querySelector(close).style.filter = "none";
-      }
+function changeLabels(state) {
+  if (!checkExistence(labelAreaSelector) || !checkExistence(labelSelector)) return
 
-      if (document.querySelector(table) !== null) document.querySelector(table).style.backgroundColor = dark;
+  document.documentElement.style.setProperty('--labelDisplay', 'flex')
+  document.querySelector(labelAreaSelector).style.display = 'var(--labelDisplay)'
+  document.documentElement.style.setProperty('--labelColor', 'black')
+  document.querySelector(labelSelector).style.color = 'var(--labelColor)'
+  document.documentElement.style.setProperty('--labelBackgroundColor', 'white')
+  document.querySelector(labelSelector).style.background = 'var(--labelBackgroundColor)'
 
-      if (settings !== null) {
-        settings.style.filter = "invert(1)";
-        info.style.filter = "none";
-      }
-    }
+  if (state == 'dark_label') {
+    document.documentElement.style.setProperty('--labelBackgroundColor', labelBackgroundColor)
+    document.documentElement.style.setProperty('--labelColor', 'white')
+  }
+  else if (state == 'no_label') {
+    document.documentElement.style.setProperty('--labelDisplay', 'none')
+  }
+}
+
+function manageFlag(state) {
+  clearInterval(manageFlagInterval)
+  manageFlagInterval = setInterval(changeFlag, 100, state);
+}
+
+function changeFlag(state) {
+  if (!checkExistence(cornerFlag)) return
+
+  if (state) {
+    document.querySelector(cornerFlag).style.display = 'none'
   }
   else {
-    backgroundBorders.style.background = white;
-    backgroundBorders.style.color = black;
-    mainBackground.style.background = white;
+    document.querySelector(cornerFlag).style.display = 'flex'
+  }
+}
 
-    root.style.colorScheme = "light";
+function manageHeader(state) {
+  clearInterval(manageHeaderInterval)
+  manageHeaderInterval = setInterval(changeHeader, 100, state);
+}
 
-    blueLinks.forEach((link) => link.style.color = "var(--seterra-color-link)");
-    buttons.forEach((button) => {
-      button.style.color = "var(--ds-color-black-90)";
-      button.style.border = ".0625rem solid rgb(255 255 255 / 90%)"
-    })
+function changeHeader(state) {
+  if (!checkExistence(quizHeader)) return
 
+  if (state) {
+    document.querySelector(quizHeader).style.backgroundColor = 'rgb(0 0 0 / 40%)'
+    document.querySelector(quizHeader).style.color = 'white'
+  }
+  else {
+    document.querySelector(quizHeader).style.backgroundColor = 'rgb(255 255 255 / 50%)'
+    document.querySelector(quizHeader).style.color = 'black'
+  }
+}
 
-    if (fromTimer === true) {
-      const buttonType = ".game-type-map-input_button__1K9uM"
-      const modal = ".modal_colorWhite__4hqsv";
-      const close = ".modal_closeButton__rq9h_ img";
-      const table = ".highscore_table__MNojq";
+function manageTheme(state) {
+  clearInterval(manageThemeBigInterval)
+  clearInterval(manageThemeSmallInterval)
+  manageThemeBigInterval = setInterval(changeThemeConstant, 300, state)
+  manageThemeSmallInterval = setInterval(changeThemeVariables, 100, state)
+}
 
-      if (document.querySelector(buttonType) !== null) document.querySelector(buttonType).style.color = white;
+function changeThemeConstant(state) {
+  document.documentElement.style.setProperty('--originallyLight', 'white')
+  let originallyLight = '.seterra_content__IQ6lE,.seterra_main__EGcCa'
+  document.querySelectorAll(originallyLight).forEach(element => {
+    element.style.backgroundColor = 'var(--originallyLight)'
+  })
 
-      if (document.querySelector(modal) !== null) {
-        document.querySelector(modal).style.background = white;
-        document.querySelector(modal).style.color = black;
-        document.querySelector(close).style.filter = "invert(1)";
-      }
+  document.documentElement.style.setProperty('--originallyDark', 'black')
+  let originallyDark = 'body,.seterra_root__5mvZs,.button_button__CnARx.button_variantSecondaryInverted__9kX_w,.button_link__xHa3x.button_variantSecondaryInverted__9kX_w'
+  document.querySelectorAll(originallyDark).forEach(element => {
+    element.style.color = 'var(--originallyDark)'
+  })
 
-      if (document.querySelector(table) !== null) document.querySelector(table).style.backgroundColor = white;
+  document.documentElement.style.setProperty('--links', 'var(--seterra-color-links)')
+  document.querySelectorAll('.games-list_linkItem__V_M6y a').forEach(link => {
+    link.style.color = 'var(--links)'
+  })
 
-      if (settings !== null) {
-        settings.style.filter = "none";
-        info.style.filter = "brightness(100) invert(100%)";
-      }
+  document.documentElement.style.setProperty('--gray', '#ebebeb')
+
+  document.querySelectorAll('.button_button__CnARx.button_variantSecondaryInverted__9kX_w, .button_link__xHa3x.button_variantSecondaryInverted__9kX_w').forEach(button => {
+    button.style.border = '.0625rem solid var(--originallyDark)'
+  })
+
+  if (state) {
+    document.documentElement.style.setProperty('--originallyLight', background)
+    document.documentElement.style.setProperty('--originallyDark', 'white')
+    document.documentElement.style.setProperty('--links', lightGray)
+    document.documentElement.style.setProperty('--gray', darkGray)
+  }
+  else {
+    document.documentElement.style.setProperty('--originallyLight', 'white')
+    document.documentElement.style.setProperty('--originallyDark', background)
+    document.documentElement.style.setProperty('--links', 'var(--seterra-color-links)')
+    document.documentElement.style.setProperty('--gray', gray)
+  }
+}
+
+function changeThemeVariables(state) {
+  if (checkExistence('.game-footer_settingsButton__6xv0w')) {
+    if (state) {
+      document.querySelector('.game-footer_settingsButton__6xv0w').style.filter = 'none'
+    }
+    else {
+      document.querySelector('.game-footer_settingsButton__6xv0w').style.filter = 'invert(1)'
     }
   }
-}
 
-function manageWater(value) {
-  const water = document.querySelector(".map-styles_water__W49Gr");
-  if (water !== null) water.style.fill = value;
-}
-
-function onResponseNoLabel(item) {
-  // if no_label is true it'll remove. else it'll add label 
-  manageLabel(item.no_label);
-}
-
-function onResponseDarkLabel(item) {
-  // if dark_label is true it'll change label to dark. else it'll change label to light
-  manageDarkLabel(item.dark_label);
-}
-
-function onResponseDarkMode(item) {
-  let sendFromTimer = true;
-  // if darkmode is true, it'll add dark mode. else it'll bring back light mode
-  manageDarkMode(item.dark_mode, sendFromTimer);
-}
-
-function onReject(error) {
-  // if error in response
-  console.error(error);
-}
-
-// WARNING! SETTED MANUALLY!
-setInterval(() => {
-  const storageDarkHeader = chrome.storage.local.get("dark_quiz_header")
-  .then((item) => manageHeader(item.dark_quiz_header))
-
-  const storageCornerFlag = chrome.storage.local.get("no_corner_flag")
-  .then((item) => manageCornerFlag(item.no_corner_flag));
-
-  const storageCustomWater = chrome.storage.local.get("custom_water")
-  .then((item) => manageWater(item.custom_water));
-
-  const header = document.querySelector(".game-header_wrapper__82t4V");
-  if (darkHeader === false && header !== null) {
-    header.style.color = "black";
-    header.style.background = "hsla(0,0%,100%,.5)";
-  }
-  else if (darkHeader === true && header !== null) {
-    header.style.color = "white";
-    header.style.background = "hsla(0,0%,0%,.3)";
+  if (checkExistence('.highscore_table__MNojq')) {
+    document.querySelector('.highscore_table__MNojq').style.backgroundColor = 'var(--originallyLight)'
   }
 
+  if (checkExistence('.modal_colorWhite__4hqsv')) {
+    document.querySelector('.modal_colorWhite__4hqsv').style.color = 'var(--originallyDark)'
+    document.querySelector('.modal_colorWhite__4hqsv').style.backgroundColor = 'var(--originallyLight)'
+    document.querySelector('.modal_closeButton__rq9h_').style.border = '.0625rem solid var(--Dark)'
+    if (state) {
+      document.querySelector('.modal_closeButton__rq9h_').style.filter = 'invert(1)'
+    }
+    else {
+      document.querySelector('.modal_closeButton__rq9h_').style.filter = 'none'
+    }
+    if (checkExistence('[data-qa="score-modal-review-button"]')) {
+      document.querySelector('[data-qa="score-modal-review-button"]').style.color = 'var(--originallyDark)'
+      document.querySelector('[data-qa="score-modal-review-button"]').style.border = '.0625rem solid var(--originallyDark)'
+    }
+  }
 
-}, 1000);
+  if (checkExistence('.game-flags_gameAreaContainer__tU8wK')) {
+    document.querySelector('.game-flags_gameAreaContainer__tU8wK').style.backgroundColor = 'var(--gray)'
+  }
+}
 
-// WARNING! SETTED MANUALLY!
-setInterval(() => {
-  const storageCheckboxNoLabel = chrome.storage.local.get("no_label")
-  .then(onResponseNoLabel, onReject);
-  const storageDarkLabel = chrome.storage.local.get("dark_label")
-  .then(onResponseDarkLabel, onReject);
-  const storageDarkMode = chrome.storage.local.get("dark_mode")
-  .then(onResponseDarkMode, onReject);
-}, 100);
+function manageLabelOpacity(state) {
+  clearInterval(manageLabelOpacityInterval)
+  manageLabelOpacityInterval = setInterval(changeLabelOpacity, 100, state)
+}
+
+function changeLabelOpacity(state) {
+  if (checkExistence(labelSelector)) {
+    document.querySelector(labelSelector).style.opacity = state
+  }
+}
+
+function checkExistence(element) {
+  return document.querySelector(element)
+}
+
+// get all options on page load and pass it to the function
+window.addEventListener('load', getOptions)
